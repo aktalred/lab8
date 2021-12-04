ed18055480d11ed532f4aff395c453d074daa4da
Grzegorz Derlatka <grzegorzderlatka1@gmail.com>
2021-12-04 23:04:01 +0100

    add missing files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 123 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Podaj k: </p>
+                <input id='fib_k'/>
+                <p></p>
+                <button onClick={showResult}>Oblicz</button>
+                <p id='result'></p>
+            </header>
+        </div>
+    );
+}
+
+function showResult() {
+    let k = document.getElementById('fib_k').value;
+    let result = fib(k);
+    document.getElementById('result').innerHTML = 'Wynik: ' + result;
+}
+
+function fib(k) {
+    if (k < 3)
+        return 1;
+
+    return fib(k - 2) + fib(k - 1);
 }
 
 export default App;
